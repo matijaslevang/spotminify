ede598abc24f7c93407720e4af45ada14728d49a
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2025-09-13 18:22:51 +0200

    Update navigation bar and styling

--- Frontend/src/app/layout/nav-bar/nav-bar.component.ts
+++ Frontend/src/app/layout/nav-bar/nav-bar.component.ts
@@ -1,26 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit{
-  role: string | null = '';
-  constructor(private authService: AuthService, private router: Router) {
-  }
+export class NavBarComponent implements OnInit {
+  isLoggedIn: boolean = false;
+
+  constructor(private authService: AuthService, public router: Router) {}
 
   ngOnInit(): void {
-    // To-do: get the role
-    // this.authService.getRole.subscribe((result) => {
-    //   this.role = result;
-    // })
+    this.authService.getCurrentUser().subscribe({
+      next: user => {
+        this.isLoggedIn = !!user;
+      },
+      error: () => {
+        this.isLoggedIn = false;
+      }
+    });
   }
 
-  logout(): void{
-    // To do
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: () => {
+        this.isLoggedIn = false;
+        this.router.navigate(['/login']);
+      },
+      error: () => {
+        this.isLoggedIn = false;
+        this.router.navigate(['/login']);
+      }
+    });
   }
-
 }
