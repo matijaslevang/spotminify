ff12fe2067f4c25000677a4cc190c0ceb32ba786
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2025-09-13 18:20:47 +0200

    Add amplify configuration to main and update environment

--- Frontend/src/enviroment.ts
+++ Frontend/src/enviroment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   cognito: {
-    userPoolId: 'eu-central-1_4MtMLqQeE',
-    clientId: '69569812qt2j3rblsamnphhs1u',
+    userPoolId: 'eu-central-1_kT8FCycuf',
+    clientId: '154ia7nn1gp6ee2a9jv72kvqu',
     region: 'eu-central-1'
   },
-  apiUrl: 'https://53hzf6rmba.execute-api.eu-central-1.amazonaws.com/prod/'
+  apiUrl: 'https://u5wwaa29bc.execute-api.eu-central-1.amazonaws.com/prod/'
 };
