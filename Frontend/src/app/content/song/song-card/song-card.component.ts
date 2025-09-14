e3026b9a8027a399a605dc0f6cfd765282164e7f
Matijas Levang <micolevang@gmail.com>
2025-09-14 19:49:42 +0200

    Add song card

--- Frontend/src/app/content/song/song-card/song-card.component.ts
+++ Frontend/src/app/content/song/song-card/song-card.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Song } from '../../models/model';
+import { FastAverageColor } from 'fast-average-color';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-song-card',
   templateUrl: './song-card.component.html',
   styleUrl: './song-card.component.css'
 })
-export class SongCardComponent {
+export class SongCardComponent implements AfterViewInit {
+  @ViewChild('songCardImage') songImageRef!: ElementRef<HTMLImageElement>;
+  @Input() song: Song;
 
+  dominantColor: string = "#ffffff";
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    
+  }
+
+  ngAfterViewInit(): void {
+    const img = this.songImageRef.nativeElement
+    img.crossOrigin = 'anonymous'
+
+    img.onload = () => {
+      const fac = new FastAverageColor();
+      const result = fac.getColor(img);
+      this.dominantColor = result.hex;
+    }
+  }
+
+  viewSong(): void {
+    this.router.navigate(["/song"], {state: { songName: this.song.name}});
+  }
 }
