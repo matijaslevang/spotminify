341e664a9ae929310eff429d2b5470e0054dc148
Matijas Levang <micolevang@gmail.com>
2025-09-17 19:22:01 +0200

    Add discover page to routing and navbar

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ArtistViewComponent } from './content/artist/artist-view/artist-view.co
 import { SongViewComponent } from './content/song/song-view/song-view.component';
 import { AlbumViewComponent } from './content/album/album-view/album-view.component';
 import { CreateArtistComponent } from './content/artist/create-artist/create-artist.component';
+import { DiscoverPageComponent } from './layout/discover-page/discover-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -17,6 +18,7 @@ const routes: Routes = [
   { path: 'song', component: SongViewComponent },
   { path: 'album', component: AlbumViewComponent },
   { path: 'create-artist', component: CreateArtistComponent },
+  { path: 'discover', component: DiscoverPageComponent },
   { path: '**', redirectTo: 'home' },
 ];
 
