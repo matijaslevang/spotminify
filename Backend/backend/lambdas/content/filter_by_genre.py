765896641c07020cd0838c6531001f46e80868e2
Matijas Levang <micolevang@gmail.com>
2025-10-18 23:38:05 +0200

    [ADD] Filter by genre and artist functions

---
+++ Backend/backend/lambdas/content/filter_by_genre.py
@@ -0,0 +1,48 @@
+import json
+import os
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table_name = os.environ['GENRE_INDEX_TABLE']
+table = dynamodb.Table(table_name)
+
+def handler(event, context):
+    genre_name = event['queryStringParameters']['genreName']
+    
+    response = table.query(
+        KeyConditionExpression='genreName = :genreName',
+        ExpressionAttributeValues={
+            ':genreName': genre_name
+        }
+    )
+
+    result_albums = []
+    result_artists = []
+    result_songs = []
+
+    for item in response['Items']:
+        content_type = item.get('contentType')
+        
+        if content_type == 'album':
+            result_albums.append(item)
+        elif content_type == 'artist':
+            result_artists.append(item)
+        elif content_type == 'single':
+            result_songs.append(item)
+    
+    return {
+        'statusCode': 200,
+        'headers': cors_headers(),
+        'body': json.dumps({
+            'resultAlbums': result_albums,
+            'resultArtists': result_artists,
+            'resultSongs': result_songs
+        })
+    }
+
+def cors_headers():
+    return {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        "Access-Control-Allow-Methods": "OPTIONS,GET,PUT,POST,DELETE"
+    }
